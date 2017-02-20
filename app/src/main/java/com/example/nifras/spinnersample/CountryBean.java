c4b9edd0aeb64ee0df4838a93dafd2c67d807122
nifrastechorin <nifras@techorin.lk>
2017-02-20 15:42:35 +0530

    Country Spinner Added

---
+++ app/src/main/java/com/example/nifras/spinnersample/CountryBean.java
@@ -0,0 +1,33 @@
+package com.example.nifras.spinnersample;
+
+/**
+ * Created by nifras on 2/20/17.
+ */
+public class CountryBean {
+    int id;
+    String name, code;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
--- app/src/main/java/com/example/nifras/spinnersample/MainActivity.java
+++ app/src/main/java/com/example/nifras/spinnersample/MainActivity.java
@@ -1,30 +1,133 @@
 package com.example.nifras.spinnersample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    JSONArray jsonTemp;
+    private ArrayList<String> list;
+    private Spinner spinner;
+    private ArrayAdapter<String> adp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner spinner = (Spinner)findViewById(R.id.spinner);
+        spinner = (Spinner) findViewById(R.id.spinner);
 
-        ArrayList<String> list;
+
         list = new ArrayList<String>();
-        for (int i = 0; i <25; i++) {
-            list.add(i + " Item");
-        }
-
-        ArrayAdapter<String> adp = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, list);
+        adp = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
         adp.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adp);
+        LoadCountries();
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    private void LoadCountries() {
+        OkHttpClient client = new OkHttpClient();
+
+        okhttp3.Request request = new Request.Builder()
+                .url("https://demo8816124.mockable.io/spinner_sample")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, final IOException ex) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    String responseString = response.body().string();
+
+                    JSONArray jsonArray = new JSONArray(responseString);
+                    jsonTemp = new JSONArray(responseString);
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        Gson gson = new Gson();
+                        final CountryBean countryBean = gson.fromJson(jsonObject.toString(), CountryBean.class);
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                updateUI(countryBean);
+                            }
+                        });
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+    }
+
+    private void updateUI(CountryBean countryBean) {
+        if (countryBean != null) {
+            list.add(countryBean.getCode());
+            adp.notifyDataSetChanged();
+        }
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = jsonTemp.getJSONObject(position);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Gson gson = new Gson();
+        final CountryBean countryBean = gson.fromJson(jsonObject.toString(), CountryBean.class);
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                LoadCities(countryBean);
+            }
+        });
+
+    }
+
+    private void LoadCities(CountryBean countryBean) {
+        Toast.makeText(getApplicationContext(), countryBean.getName(), Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
